3dcb4011ad168de1b537ebaddb4c7b5d44bddf44
Vinagradov Mikhail <vinogradov.mikhail@mail.ru>
2022-06-19 13:01:48 +0300

    Squashed commit of the following:
    commit 11c5640ce7789ece721c79ddc78a03771f8f5e3d
    Author: Vinagradov Mikhail <vinogradov.mikhail@mail.ru>
    Date:   Sun Jun 19 12:58:47 2022 +0300
    
        Update Main.java
    
        description

--- Main.java
+++ Main.java
@@ -4,21 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ComplexNumber b = new ComplexNumber(0, 3);
-        ComplexNumber c = new ComplexNumber(17, 5);
-        ComplexNumber res = b.add(c);
-        System.out.println("Result is = " + res);
     }
 
     public static void chisla(int start, int end) {
-        if (start <= end) {
-            if (start % 2 == 0)
-                System.out.println(start + " Число четное");
-            chisla(++start, end);
-        }
+    }
 
-        int a = 3;
-
+    public void printer(){
     }
 }
 
